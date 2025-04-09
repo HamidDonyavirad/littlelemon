544a79f26af79c2dc2ded720cd14c631439ea676
HamidDonyavirad <donyavirad76@gmail.com>
2025-04-09 10:42:13 +0330

    Set up the menu API

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -20,5 +20,7 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('restaurant/', include('restaurant.urls'))
+    path('restaurant/', include('restaurant.urls')),
+    path('restaurant/menu/',include('restaurant.urls')),
+    
 ]
