fe2dc539acf0783ee461af90ad4d14ad5e5fc8fd
HamidDonyavirad <donyavirad76@gmail.com>
2025-04-11 10:46:15 +0330

    Securing the table booking API

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics , viewsets
 from .models import Menu , Booking
 from .serializers import MenuSerializer , BookingSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
@@ -18,5 +19,6 @@ class singleMenuItamView (generics.RetrieveUpdateAPIView,generics.DestroyAPIView
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
     
     
