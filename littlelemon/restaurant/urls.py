3f2c97477f735dac663ee72e261a76d2a1e7d9ac
HamidDonyavirad <donyavirad76@gmail.com>
2025-04-12 10:22:25 +0330

    Modify the two project and app urls.py files

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -4,8 +4,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('menu/',views.MenuItemsView.as_view()),
-    path('menu/<int:pk>',views.singleMenuItamView.as_view()),
-    path('menu/items/', views.MenuItemsView.as_view()),
+    path('api/menu/',views.MenuItemsView.as_view()),
+    path('api/menu/<int:pk>',views.singleMenuItamView.as_view()),
     path('api-token-auth/',obtain_auth_token),
 ]
